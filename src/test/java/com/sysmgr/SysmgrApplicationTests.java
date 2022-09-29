22864899f59fdb846c5c18bff12cf83ec31b91a9
earlyou <earlyou1@gmail.com>
2022-09-30 01:24:14 +0900

    first commit

---
+++ src/test/java/com/sysmgr/SysmgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysmgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysmgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
